cf8117c8c6a093a77fac0e54cd7de520fdb3072f
jenil-doshi <jenil.doshi@sjsu.edu>
2016-05-04 15:22:16 -0700

    adding request object and default api

--- src/main/java/com/sjsu/cmpe277/controller/UserTypeController.java
+++ src/main/java/com/sjsu/cmpe277/controller/UserTypeController.java
@@ -1,30 +1,30 @@
 package com.sjsu.cmpe277.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sjsu.cmpe277.model.UserType;
 import com.sjsu.cmpe277.service.UserTypeService;
- 
+
 @Controller
-@RequestMapping("/")
 public class UserTypeController {
 
 	@Autowired
 	UserTypeService userTypeService;
-	
-	@RequestMapping(value = "/user/{emailId}/{userType}", method = RequestMethod.POST, produces = "application/json")
-	
-    public @ResponseBody UserType insertUserType(@PathVariable("emailId") String emailId, @PathVariable("userType") String userType ) {
-        UserType user = userTypeService.insertUserType(emailId, userType);
-        if(user==null)
-        	return null;
-        else
-        	return user;
-    }
-	
+
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody String homepage() {
+		return "{ 'response' : 'success' }";
+	}
+
+	@RequestMapping(value = "/userType", method = RequestMethod.POST, consumes="application/json", headers = "content-type=application/x-www-form-urlencoded")
+	public @ResponseBody UserType insertUserType(HttpServletRequest request) {
+		return userTypeService.insertUserType(request.getParameter("emailId"),request.getParameter("userType"));
+	}
+
 }
