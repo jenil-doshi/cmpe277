29d425ff75c39364bc829871f7290833663b5deb
maulindalal <maulin1103@gmail.com>
2016-05-08 11:16:44 -0700

    Save Search - Post

--- src/main/java/com/sjsu/cmpe277/controller/SearchController.java
+++ src/main/java/com/sjsu/cmpe277/controller/SearchController.java
@@ -1,8 +1,15 @@
 package com.sjsu.cmpe277.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sjsu.cmpe277.model.Search;
 import com.sjsu.cmpe277.service.SearchService;
 
 @Controller
@@ -11,4 +18,10 @@ public class SearchController {
 	@Autowired
 	SearchService searchService;
 	
+	@RequestMapping(value = "/search", method = RequestMethod.POST, consumes="application/json")
+	public @ResponseBody ResponseEntity<Search> insertsearchResult(@RequestBody Search search) {
+		Search searchObj = searchService.insertsearchResult(search);
+		return new ResponseEntity<Search>(searchObj, HttpStatus.OK);
+	
+}
 }
